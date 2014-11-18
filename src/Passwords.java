6cf2935d433fb27300ed0c1b40acec776c081f9c
Rohit Pattanaik <rohit.pattanaik@utexas.edu>
2014-11-17 20:52:19 -0600

    output for followers table. also writes followers to file

--- src/Passwords.java
+++ src/Passwords.java
@@ -4,13 +4,24 @@ public class Passwords {
     private static String fileName;
     private static int passwordLength;
     private static int noOfPasswords;
+    private static Table table;
 
     public static void main(String[] args) {
+
         if(!parseArgs(args)) {
             return;
         }
 
-
+        table = new Table();
+        table.generateTable(fileName);
+        table.printFollowers();
+        System.out.println();
+        table.printCounts();
+        System.out.println();
+        table.printStarters();
+        if(!table.writeToFile(false)) {
+            System.out.println("write failed");
+        }
     }
 
     private static boolean parseArgs(String[] args) {
